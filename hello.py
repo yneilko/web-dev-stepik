3f16e93677c08531f2ae72096895d615ae40c680
yneilko <y.neilko@gmail.com>
2017-04-28 12:24:10 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
 #wsgi example app
-from urlparse import parse_qs
+from cgi import parse_qs
 
-def application(environ, start_responce):
-
-        query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-        body = []
-        for key, values in query.items():
-            for items in values:
-                body.append(key + "=" + item + "r\n")
-        status = '200 OK'
-        headers = ['Content-Type', 'text/plain']
-        start_responce(status,headers)
-        return body
+def app(environ, start_responce):
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    body = []
+    for key, values in query.items():
+        for item in values:
+            body.append(key + "=" + item + "\r\n")
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_responce(status,headers)
+    return body
