c0f8c8d8979e99060180d0e88e6cf90c5efaab4f
yneilko <y.neilko@gmail.com>
2017-04-28 12:20:22 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo nginx -t
+#sudo nginx -t
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
